404d4a009e681ae6c2a6668d5f218b73721c071d
god9599 <59276134+god9599@users.noreply.github.com>
2021-07-12 02:18:39 +0900

    [Etc] 설정 및 실습

---
+++ src/main/java/com/joo/book/springboot/web/HelloController.java
@@ -0,0 +1,20 @@
+package com.joo.book.springboot.web;
+
+import com.joo.book.springboot.web.dto.HelloResponseDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // 컨트롤러를 JSON으로 반환하는 컨트롤러로 만들어준다.
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount){
+        return new HelloResponseDto(name, amount);
+    }
+}
+
---
+++ src/test/java/com/joo/book/springboot/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,21 @@
+package com.joo.book.springboot.web.dto;
+
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDtoTest {
+    @Test
+    public void lombok_test(){
+        //given
+        String name = "joo";
+        int amount = 100;
+
+        //when
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        //then
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+}
+
