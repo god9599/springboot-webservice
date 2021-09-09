aa4d1f70bb4232e9d94760be800c866fb4a1fa73
SungMin Joo <59276134+god9599@users.noreply.github.com>
2021-09-09 13:02:56 +0900

    Fix: role에 따른 접근 경로 수정

--- src/main/java/com/joo/book/springboot/config/auth/SecurityConfig.java
+++ src/main/java/com/joo/book/springboot/config/auth/SecurityConfig.java
@@ -2,10 +2,12 @@ package com.joo.book.springboot.config.auth;
 
 import com.joo.book.springboot.domain.user.Role;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Configuration
 @RequiredArgsConstructor
 @EnableWebSecurity // 스프링 시큐리티 설정 활성화
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -21,7 +23,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 // url별 권한 관리 설정
                 .authorizeRequests()
                 .antMatchers("/", "/css/**", "/images/**", "js/**", "/h2-console/**").permitAll()
-                .antMatchers("api/v1/**").hasRole(Role.USER.name())
+                .antMatchers("/api/v1/**").hasRole(Role.USER.name())
                 //나머지 url - 인증된 사용자들에게만 허용
                 .anyRequest().authenticated()
                 .and()
@@ -35,6 +37,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 // 소셜 로그인 성공하고 나서 후속 조치를 진행할 UserService 인터페이스의 구현체를 등록
                 // 로그인하고 사용자 정보 가져온 상태에서 추가로 진행하고자 하는 기능을 지원
                 .userService(customOAuth2UserService);
-
     }
 }
