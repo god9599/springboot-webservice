4957abb5889e542640a802992e1a6210ed53e2ea
SungMin Joo <59276134+god9599@users.noreply.github.com>
2021-11-16 14:30:42 +0900

    [Chore] Optional 클래스로 변경

--- src/main/java/com/joo/book/springboot/domain/messages/MsgObejctRepository.java
+++ src/main/java/com/joo/book/springboot/domain/messages/MsgObejctRepository.java
@@ -2,6 +2,8 @@ package com.joo.book.springboot.domain.messages;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MsgObejctRepository extends JpaRepository<MsgObject, MsgObjectPk> {
-    MsgObject findByMsgLangAndMsgCode(String msgLang, String msgCode);
+    Optional<MsgObject> findByMsgLangAndMsgCode(String msgLang, String msgCode);
 }
