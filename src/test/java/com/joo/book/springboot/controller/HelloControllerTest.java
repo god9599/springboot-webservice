98678b2c08fe3327359ead2c081572cc85df7807
SungMin Joo <59276134+god9599@users.noreply.github.com>
2021-11-16 14:32:58 +0900

    [Feat] Rest Docs test

--- src/test/java/com/joo/book/springboot/controller/HelloControllerTest.java
+++ src/test/java/com/joo/book/springboot/controller/HelloControllerTest.java
@@ -2,31 +2,47 @@ package com.joo.book.springboot.controller;
 
 import com.joo.book.springboot.config.auth.SecurityConfig;
 import com.joo.book.springboot.controller.HelloController;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-@ExtendWith(SpringExtension.class)
+@ExtendWith(RestDocumentationExtension.class)
 @WebMvcTest(controllers = HelloController.class, excludeFilters = {
         @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
 })
 public class HelloControllerTest {
+
     @Autowired
     private MockMvc mvc; // 웹 API를 테스트할 때 사용. 스프링 MVC 테스트의 시작점.
+
+    @BeforeEach
+    void setup(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) {
+        this.mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .apply(springSecurity())    // springSecurity설정이 되어있지 않으면 생략
+                .apply(documentationConfiguration(restDocumentation)).build();
+    }
 
     @WithMockUser(roles = "USER")
     @Test
@@ -35,6 +51,8 @@ public class HelloControllerTest {
 
         mvc.perform(get("/hello"))
                 .andExpect(status().isOk())
+                .andDo(print())
+                .andDo(document("hello"))
                 .andExpect(content().string(hello));
     }
 
